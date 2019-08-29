b38754d1a158b25098adec2301a7f1ebebb8d740
Cathy Chen <chencfb@bc.edu>
2019-08-29 10:49:18 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by 陈涵茜 on 2019/8/29.
+//  Copyright © 2019 Cathy Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
